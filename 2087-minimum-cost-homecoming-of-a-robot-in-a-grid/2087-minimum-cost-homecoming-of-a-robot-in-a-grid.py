80cbfad2d7225ff3d3c07249cbb5aa0bbbcbb1a3
Thanh Le <thanh@thanhle.ca>
2024-01-22 00:54:22 -0500

    Time: 1037 ms (100.00%), Space: 31.5 MB (62.31%) - LeetHub

--- 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.py
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.py
@@ -3,11 +3,11 @@ class Solution:
         r, c = 0, 0
         if startPos[0] < homePos[0]:
             r = sum(rowCosts[startPos[0]+1:homePos[0]+1])
-        elif startPos[0] > homePos[0]:
+        else:
             r = sum(rowCosts[homePos[0]:startPos[0]])
         if startPos[1] < homePos[1]:
             c = sum(colCosts[startPos[1]+1:homePos[1]+1])
-        elif startPos[1] > homePos[1]:
+        else:
             c = sum(colCosts[homePos[1]:startPos[1]])
         return r + c
         
