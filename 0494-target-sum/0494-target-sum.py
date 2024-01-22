9cc43a90ace93f3e71954be30b5e820c900e0e46
Thanh Le <thanh@thanhle.ca>
2024-01-22 02:04:53 -0500

    Time: 62 ms (97.90%), Space: 16.7 MB (89.40%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -3,9 +3,10 @@ class Solution:
         s = sum(nums)
         if abs(target) > s:
             return 0
-        if (s + target) % 2 == 1:
+        target = s + target
+        if target % 2 == 1:
             return 0
-        target = (s + target) // 2
+        target //= 2
         dp = [1] + [0] * target
         for num in nums:
             for i in range(target, num-1, -1):
